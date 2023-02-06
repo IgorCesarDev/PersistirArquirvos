e687c00dd8ce3b82512c425cbc60cd0b0813f793
IgorKRT <c.igor@live.com>
2023-02-06 15:05:16 -0300

    removidos codigo de testes

--- Service/FilmeService.cs
+++ Service/FilmeService.cs
@@ -37,53 +37,19 @@ namespace PersistirArquirvos.Service
                     {
                         filmeAtual.Generos.Add(generoExistente);
                         generoExistente.Filmes.Add(filmeAtual);
-                        Console.WriteLine("f");
                     }
                     else
                     {
                         filmeAtual.Generos.Add(generoAtual);
                         generoAtual.Filmes.Add(filmeAtual);
                         _context.Generos.Add(generoAtual);
-                        Console.WriteLine("t");
                     }
                     _context.Filmes.Add(filmeAtual);
                     novosFilmes.Add(filmeAtual);
                 }
-                Console.WriteLine("roda");
             });
             await _context.AddRangeAsync(novosFilmes);
-           await _context.SaveChangesAsync();
+            await _context.SaveChangesAsync();
         }
-
-
-        //foreach (string line in lines)
-        //{
-        //    Filme filmeAtual = new Filme(line);
-        //    Genero generoAtual = new Genero(line);
-
-        //    Filme filmeExistente = _context.Filmes.FirstOrDefault(f => f.Titulo == filmeAtual.Titulo);
-
-        //    if (filmeExistente == null)
-        //    {
-        //        Genero generoExistente = _context.Generos.FirstOrDefault(g => g.Nome == generoAtual.Nome);
-        //        if (generoExistente != null)
-        //        {
-        //            filmeAtual.Generos.Add(generoExistente);
-        //            generoExistente.Filmes.Add(filmeAtual);
-        //            Console.WriteLine("s");
-        //        }
-        //        else
-        //        {
-        //            filmeAtual.Generos.Add(generoAtual);
-        //            generoAtual.Filmes.Add(filmeAtual);
-        //            _context.Generos.Add(generoAtual);
-        //            Console.WriteLine("a");
-        //        }
-        //        _context.Filmes.Add(filmeAtual);
-        //    }
-        //    Console.WriteLine("f");
-        //}
-        //_context.SaveChanges();
-
     }
 }
