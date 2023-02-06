6e0249d1b35fd9c90f1582a94a8b6c7f8cc8792a
IgorKRT <c.igor@live.com>
2023-02-06 19:53:58 -0300

    att

--- Service/FilmeService.cs
+++ Service/FilmeService.cs
@@ -43,6 +43,7 @@ namespace PersistirArquirvos.Service
                         filmeAtual.Generos.Add(generoAtual);
                     }
                     novosFilmes.Add(filmeAtual);
+                    Console.WriteLine("teeste");
                 }
             });
             await _context.AddRangeAsync(novosFilmes);
